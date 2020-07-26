8db97d1b608225795b6f5a122dcdb907c0d74c18
vlad196 <vlad1.96@mail.ru>
2020-07-26 12:21:09 +0200

    I create class and precimens of dogs

---
+++ Classes.py
@@ -0,0 +1,26 @@
+#Классы
+class Dog():
+    """Пробная модель собаки"""
+
+    def __init__(self, name, age):
+        """Инициализируем атрибуты"""
+        self.name = name
+        self.age = age
+        print("Собака создана")
+
+    def sit(self):
+        """Собака будет садиться по команде"""
+        print(self.name.title() + " села")
+
+    def jump(self):
+        """Собака будет прыгать по команде"""
+        print(self.name.title() + " прыгнула")
+
+my_dog = Dog("Topik", 4)
+my_dog_2 = Dog("nick", 7)
+
+print(my_dog.age)
+print(my_dog.name)
+
+my_dog.jump()
+my_dog_2.sit()
